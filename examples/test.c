397d1c77ab4bbba66f9c723cb9d9b4393ee645bd
Cinder <cinder@cinderblocks.biz>
2021-08-30 17:35:57 -0500

    Wire up building the example C program

--- examples/test.c
+++ examples/test.c
@@ -1,29 +1,30 @@
 #define UNICODE
+
+#include "../include/UniversalSpeech.h"
+
 #include<stdio.h>
 #include<wchar.h>
 #include<windows.h> // Sleep
-#include "../include/UniversalSpeech.h"
 
-int main (int argc, char** argv) {
-speechSetValue(SP_ENABLE_NATIVE_SPEECH, TRUE);
+int main(int argc, char** argv) {
+	speechSetValue(SP_ENABLE_NATIVE_SPEECH, TRUE);
 
-printf("Here is a list of supported engines : \r\n");
-printf("%-6.6s. %-50.50s, %-10.10s\r\n", "ID", "Name", "AVailable");
+	printf("Here is a list of supported engines : \r\n");
+	printf("%-6.6s. %-50.50s, %-10.10s\r\n", "ID", "Name", "Available");
 
-const wchar_t* name;
-int i;
-for (i=0; name = speechGetString(SP_ENGINE+i); i++) {
-int avail = speechGetValue(SP_ENGINE_AVAILABLE+i);
-printf("%-5d. %-50.50ls, %-10.10s\r\n", i, name, avail?"yes":"no");
+	const wchar_t* name;
+	int i;
+	for (i = 0; name = speechGetString(SP_ENGINE + i); i++) {
+		int avail = speechGetValue(SP_ENGINE_AVAILABLE + i);
+		printf("%-5d. %-50.50ls, %-10.10s\r\n", i, name, avail ? "yes" : "no");
+	}
+
+	while (1) {
+		wchar_t msg[256];
+		const wchar_t* engineName = speechGetString(SP_ENGINE + speechGetValue(SP_ENGINE));
+		_snwprintf(msg, 255, L"You are using %s.", engineName);
+		speechSay(msg, TRUE);
+		Sleep(3000);
+	}
+	return 0;
 }
-
-while(1){
-wchar_t msg[256];
-const wchar_t* engineName = speechGetString(SP_ENGINE + speechGetValue(SP_ENGINE));
-snwprintf(msg, 255, L"You are using %s.", engineName);
-speechSay(msg, TRUE);
-Sleep(3000);
-}
-return 0;
-}
-
